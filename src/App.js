eff19155ce6aa9b806f1e3d3c56fadc953030463
camilevahviraki <camilevahviraki@gmail.com>
2024-02-07 01:24:32 +0200

    Add gmail authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import FacebookLoginButton from './authentication/facebookButton';
+import FacebookLoginButton from './authentication/facebook/facebookButton';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import GmailLogin from './authentication/gmail/gmailButton';
 import './App.css';
 
 function App() {
+
+  // firebase.initializeApp(firebaseConfig);
+  const clientId = "480336830287-gu1928tn4tefcogn1501qa06eb2lofhu.apps.googleusercontent.com";
   return (
-    <div className="App">
-       <FacebookLoginButton/>
-    </div>
+    <GoogleOAuthProvider clientId={clientId}>
+      <div className="App">
+        <FacebookLoginButton />
+        <GmailLogin />
+      </div>
+    </GoogleOAuthProvider>
+
   );
 }
 
